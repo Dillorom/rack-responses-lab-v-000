64ca64663dd015d2f2c9101f48e6e713c5ec3ba1
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-06-28 19:56:09 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,8 +3,8 @@ class Application
     resp = Rack::Response.new
 
     time = Time.now
-    binding.pry
-    if time.hour >= 12
+    #binding.pry
+    if time.hour <= 12
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"

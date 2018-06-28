53a04adb876158d8eae60123585064a2a0914dce
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-06-28 19:34:24 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
-    if currenttime.between?(1...12)
+    if Time.now.between?(1...12)
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"
